329ef8a36a76a28b5555892bd0bcaa0a93d109de
guimaaz <gustavoguimaraesr@gmail.com>
2026-04-16 16:57:49 +0200

    update : ingest.py ready

--- ingest.py
+++ ingest.py
@@ -4,29 +4,41 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 
 
-Data_path = "./backend/data/md/"
+Data_path = "./backend/Data/Md/"
 Db_path = "./backend/db"
 
 def preparar_banco():
-    loader = DirectoryLoader(
-        Data_path, 
-        glob="./*.md", 
-        loader_cls=UnstructuredMarkdownLoader
-    )
+    try :
+        loader = DirectoryLoader(
+            Data_path, 
+            glob="*.md", 
+            loader_cls=UnstructuredMarkdownLoader
+        )
+    except  Exception  as e : 
+        print(f"erro no Loader, não foi possivel achar pastar em formato .md, ou houve algum erro com o formato do leitor {e}")
     
     documentos = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500, 
         chunk_overlap=50
     )
-    fragmentos = text_splitter.split_documents(documentos)
+
+    try : 
+        fragmentos = text_splitter.split_documents(documentos)
+    except Exception as e :
+        print(f"erro no chunking de documentos {e}")
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    vectorstore = Chroma.from_documents(
-        documents=fragmentos,
-        embedding=embeddings,
-        persist_directory=Db_path,
-        collection_name="ayron_lab"
-    )
+
+    try :
+        vectorstore = Chroma.from_documents(
+            documents=fragmentos,
+            embedding=embeddings,
+            persist_directory=Db_path,
+            collection_name="verso"
+        )
+    except Exception as e :
+        print(f"erro ao gerar o vector store {e}")
+        
 
 if __name__ == "__main__":
     preparar_banco()
